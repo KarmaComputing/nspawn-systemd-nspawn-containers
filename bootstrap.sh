30d6559417c0558d4b8c535a0de7882e705fb293
chrisjsimpson <chris.j.simpson@live.co.uk>
2023-02-08 22:58:10 +0000

    automate initial password

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ HOST_IP=$1
 FLOATING_IP=$2
 
 apt update
-apt install -y systemd-container debootstrap bridge-utils tmux telnet traceroute vim python3 qemu-guest-agent
+apt install -y systemd-container debootstrap bridge-utils tmux telnet traceroute vim python3 python3-venv qemu-guest-agent
 echo "set mouse=" > ~/.vimrc
 echo 'kernel.unprivileged_userns_clone=1' >/etc/sysctl.d/nspawn.conf
 # allow proxy_arp
@@ -22,7 +22,7 @@ mkdir -p /etc/systemd/nspawn
 
 # Create a Debian nspawn container using debootstrap
 
-debootstrap --include=systemd,dbus,traceroute,telnet,curl stable /var/lib/machines/debian
+debootstrap --include=systemd,dbus,traceroute,telnet,curl,python3 stable /var/lib/machines/debian
 
 cat >/etc/systemd/nspawn/debian.nspawn <<EOL
 [Network]
@@ -124,29 +124,70 @@ HOST_PUBLIC_KEY=$(cat /root/.ssh/id_ecdsa.pub)
 # Add public key to guest authorized_keys
 echo -e "$HOST_PUBLIC_KEY" >> /var/lib/machines/debian/root/.ssh/authorized_keys
 
+# Build an ansible inventory for setting guests password
+# to overcome Authentication token manipulation errors. TODO work out so
+# can drop ansible.
+python3 -m venv venv
+. venv/bin/activate
+pip install ansible
+echo "$FLOATING_IP ansible_user=root" > inventory
+
+# Write a playbook
+PASSWORD=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 15 ; echo '')
+
+cat >change_password.yml <<EOL
+---
+- name: set guest ssh password
+  hosts: all
+  become: true
+  vars:
+    username: "root"
+    password: "$PASSWORD"
+  tasks:
+    - name: change password
+      ansible.builtin.user:
+        name: "{{ username }}"
+        state: present
+        password: "{{ password | password_hash('sha512') }}"
+EOL
+
+# Set guest password
+ansible-playbook -i inventory change_password.yml
+
 
 set +x
 
 echo "#####################"
-echo Remaining manual steps:
-echo 
-echo "You'll want to ssh to the host now and perform the following:"
-echo 1. ssh to your host: 
-echo ssh root@"$HOST_IP"
-echo 
-echo 2. Set password and configure /etc/securetty:
-echo systemctl stop systemd-nspawn@debian.service
-echo systemd-nspawn -D /var/lib/machines/debian -U --machine debian passwd
-echo systemctl start systemd-nspawn@debian.service
+echo Setup complete!
 echo
-echo 3. Verify ping from your localhost:
-echo exit
+echo You may want to:
+echo - Verify you can ping your guest from your localhost:
+echo
 echo ping -c 3 "$FLOATING_IP"
 echo
-echo Trouble? "Directory tree /var/lib/machines/debian is currently busy"?
-echo 4. You may need to stop /start the container:
+echo - ssh to your host:
+echo ssh root@"$HOST_IP"
+echo
+echo "- ssh from your host ($HOST_IP) to your guest:"
+echo   ssh root@"$FLOATING_IP"
+echo
+echo - Or directly since your on the host anyway
+echo   machinectl login debian
+echo   "# The guest root password is $PASSWORD"
+echo
+echo - You can start stop guests like this:
+echo
 echo systemctl stop systemd-nspawn@debian.service
 echo systemctl start systemd-nspawn@debian.service
+echo
+echo Note: You must use ssh key based authentication to
+echo access your guest remotely over ssh.
+echo "If you want to do that, then you'll have to add your"
+echo localhost\'s public ssh key to the authorized_keys file
+echo on your guest.
+echo ssh access from your host $HOST_IP to your guest has
+echo "already been setup (see above)."
+echo
 echo "#####################"
 
 
